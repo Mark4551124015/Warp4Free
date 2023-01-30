870ce68830f09ea61adf74770f2f859e2eb1fbbb
peyton2465 <peyton2465@outlook.com>
2023-01-30 16:22:13 -0600

    Code improvements, All features enabled

--- Warp4Free/Warp4Free.cpp
+++ Warp4Free/Warp4Free.cpp
@@ -8,32 +8,33 @@
 
 #include "MinHook.h"
 
-bool compare(const uint8_t* pData, const uint8_t* bMask, const char* szMask) {
-    for (; *szMask; ++szMask, ++pData, ++bMask) {
-        if (*szMask == 'x' && *pData != *bMask) {
+bool compare(const uint8_t* address, const uint8_t* pattern, const char* mask) {
+    for (; *mask; ++mask, ++address, ++pattern) {
+        if (*mask == 'x' && *address != *pattern) {
             return 0;
         }
     }
-    return (*szMask) == NULL;
+    return (*mask) == NULL;
 }
 
-uintptr_t find_pattern(uintptr_t dwAddress, uintptr_t dwLen, uint8_t* bMask, char* szMask) {
-    for (uintptr_t i = 0; i < dwLen; i++) {
-        if (compare((uint8_t*)(dwAddress + i), bMask, szMask)) {
-            return dwAddress + i;
+uintptr_t find_pattern(uintptr_t start_address, uintptr_t length, uint8_t* pattern, char* mask) {
+    for (uintptr_t i = 0; i < length; i++) {
+        if (compare((uint8_t*)(start_address + i), pattern, mask)) {
+            return start_address + i;
         }
     }
     return 0;
 }
 
-std::vector<std::string> split_str(std::string s, char delim) {
+std::vector<std::string> split_str(std::string input, char delim) {
     std::vector<std::string> result;
-    std::istringstream string_stream;
-    string_stream.clear();
-    string_stream.str(s);
-    std::string temp;
-    while (std::getline(string_stream, temp, delim))
-        result.push_back(temp);
+    size_t length = input.length();
+    for (size_t index = 0; index < length; index++) {
+        size_t pos = input.find(delim, index);
+        result.push_back(input.substr(index, pos - index));
+        if (pos > length) break;
+        index = pos;
+    }
     return result;
 }
 
@@ -41,31 +42,31 @@ uintptr_t scan_ida(std::string ida_pattern, uintptr_t start_address, size_t leng
     std::vector<std::string> bytes = split_str(ida_pattern, ' ');
     std::string pattern = "", mask = "";
 
-    for (auto& it : bytes) {
+    for (std::string& it : bytes) {
         if (it.size() && it[0] == '?') {
             mask += '?';
             pattern += '\0';
         }
         else {
             mask += 'x';
-            pattern += (unsigned char)std::strtol(it.c_str(), NULL, 16);
+            pattern += (uint8_t)std::strtol(it.c_str(), NULL, 16);
         }
     }
 
     return find_pattern(start_address, length, (uint8_t*)pattern.c_str(), const_cast<char*>(mask.c_str()));
 }
 
-bool find_module(MODULEINFO* moduleInfo, const wchar_t* name) {
-    HMODULE hMods[1024];
-    HANDLE hProcess = GetCurrentProcess();
-    DWORD cbNeeded;
+bool find_module(MODULEINFO* module_info, const wchar_t* name) {
+    HMODULE modules[1024];
+    HANDLE process = GetCurrentProcess();
+    DWORD bytes_needed;
 
-    if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
-        for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
-            TCHAR szModName[MAX_PATH];
-            if (GetModuleFileNameEx(hProcess, hMods[i], szModName, sizeof(szModName) / sizeof(TCHAR))) {
-                if (std::wstring(szModName).find(name) != std::wstring::npos) {
-                    return GetModuleInformation(hProcess, hMods[i], moduleInfo, sizeof(MODULEINFO));
+    if (EnumProcessModules(process, modules, sizeof(modules), &bytes_needed)) {
+        for (int32_t i = 0; i < (bytes_needed / sizeof(HMODULE)); i++) {
+            TCHAR module_name[MAX_PATH];
+            if (GetModuleFileNameEx(process, modules[i], module_name, sizeof(module_name) / sizeof(TCHAR))) {
+                if (std::wstring(module_name).find(name) != std::wstring::npos) {
+                    return GetModuleInformation(process, modules[i], module_info, sizeof(MODULEINFO));
                 }
             }
         }
@@ -78,15 +79,24 @@ typedef __int64(__fastcall* t_ui_update)(__int64, int, int, float, int, __int64,
 t_ui_update h_ui_update_tramp = NULL;
 
 __int64 __fastcall h_ui_update(__int64 a1, int a2, int a3, float a4, int a5, __int64 a6, __int64 a7, void* a8, void* a9, __int64 a10, int* a11) {
-    *(__int64*)(*(__int64*)(a7 + 160)) = 0xFFFFFFFF;
+    static bool printed = false;
+    if (!printed) {
+        printed = true;
+        std::wcout << "Arg 7 Address: " << std::hex << a7 << std::dec << '\n';
+    }
+
+    auto permissions = *(__int64*)(a7 + 160);
+    *(__int64*)(permissions) = 0xFFFFFFFFFFFFFFFF;
+
     return h_ui_update_tramp(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11);
 }
 
 void start() {
 
-#ifdef DEBUG
+#ifdef _DEBUG
     AllocConsole();
-    freopen("CONOUT$", "w", stdout);
+    FILE* stream{};
+    freopen_s(&stream, "CONOUT$", "w", stdout);
 #endif
 
     std::wcout << "Warp4Free Attached\n";
@@ -100,19 +110,23 @@ void start() {
         return;
     }
 
-    std::wcout << std::hex << (uintptr_t)parsecdll.lpBaseOfDll << '\n';
+    std::wcout << std::hex << "Module Base: " << (uintptr_t)parsecdll.lpBaseOfDll << '\n';
 
     uintptr_t ui_update_ptr_call = scan_ida("E8 ? ? ? ? 0F B6 4F 2C", (uintptr_t)parsecdll.lpBaseOfDll, parsecdll.SizeOfImage);
-    uintptr_t ui_update_ptr = ui_update_ptr_call + *(uint32_t*)(ui_update_ptr_call + 1) + 5;
+    uintptr_t ui_update_ptr = ui_update_ptr_call + *(int32_t*)(ui_update_ptr_call + 1) + 5;
+
+    std::wcout << std::hex << "UI Update: " << ui_update_ptr << '\n';
 
     if (MH_Initialize() != MH_OK)
         return;
+
 
     if (MH_CreateHook((void*)ui_update_ptr, &h_ui_update, reinterpret_cast<LPVOID*>(&h_ui_update_tramp)) != MH_OK)
         return;
 
     if (MH_EnableHook((void*)ui_update_ptr) != MH_OK)
         return;
+    
 
     std::wcout << "Hooked!\n";
 }
